0ec61d57e8480d1d1cfb3f5c6507bcbc234ebd31
Alan <treanora@dnb.com>
2021-03-21 13:07:15 +0000

    added go-fetch-api project to repo

---
+++ go-fetch-api/README.md
@@ -0,0 +1,3 @@
+# go-fetch-api
+
+go-fetch-api is a JavaScript project that displays text from a text file, json file and an external Github api using the fetch method. 
---
+++ go-fetch-api/app.js
@@ -0,0 +1,77 @@
+//  ########## Event Listener for Get Text Button, calls getTest function  #########
+document.getElementById('button1').addEventListener('click', getText);
+
+//  ##########  Event Listener for Get Json Button, calls getJson function  ###########
+document.getElementById('button2').addEventListener('click', getJson);
+
+//  ########## Event Listener for Get API Data, calls getExternalApi function  ##########
+document.getElementById('button3').addEventListener('click', getExternalApi);
+
+
+// ---------  getText function  ----------
+// getText function retrieves text from local file using fetch/promises
+function getText() {
+  // call fetch function passing file, which will return a promise
+  fetch('test.txt')
+    // to get a response from a promise use .then to return text
+    .then(function(res) {
+      return res.text();
+    })
+    // use .then again to get data from returned text
+    .then(function(data){
+      console.log(data);
+      // output data to DOM
+      document.getElementById('output').innerHTML = data;
+    })
+    // use .catch to catch error & display to console
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+// ---------  getJson function  ----------
+// getJson function retrieves Json from local file using fetch/promises
+function getJson() {
+  // call fetch function passing file, which will return a promise
+  fetch('post.json')
+    // to get a response from a promise use .then to return text
+    .then(function(res) {
+      return res.json();
+    })
+    // use .then again to get data from returned text
+    .then(function(data){
+      console.log(data);
+      // output json data to DOM by iterating over it & adding as list item
+      let output = '';
+      data.forEach(function(post){
+        output+= `<li>${post.title}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    // use .catch to catch error & display to console
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+
+//  ----------  getExternalApi function  ----------
+// getExternalApi function retreives Json from external api using fetch/promises
+function getExternalApi() {
+  fetch('https://api.github.com/users')
+  .then(res => res.json())
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(data){
+    console.log(data);
+    let output = '<h5>Top 30 Github Users</h5>';
+    data.forEach(function(user){
+      output += `<li>${user.login}</li>`;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
