b54b5b58b058938c44405c118b1e8bb18a526917
atreanor <TreanorA@dnb.com>
2021-05-12 15:20:50 +0100

    initial commit

---
+++ openweather-app/weather.js
@@ -0,0 +1,13 @@
+class Weather {
+  constructor(city, state) {
+    this.apiKey = '';
+    this.city = city;
+    //this.state = state;
+  }
+
+  // fetch weather from API using asnyc
+  async getWeather() {
+    const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${this.apiKey}`)
+
+  }
+}
